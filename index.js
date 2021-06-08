a727892fd8d572ebc6ad19942433fa78c837ba31
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2021-06-09 03:32:03 +0530

    New Features :tada:
    - Retry in Fetch. The retry also goes through the throttle queue
    - Write a maker file along with the data file so we don't re-download the same file over and over again.

--- index.js
+++ index.js
@@ -39,6 +39,11 @@ if (maxDays > 60) {
     console.warn("We can't fetch data more than 60 days")
     process.exit(1)
 }
+const maxFetchRetries = argv.maxFetchRetries || 3
+if (maxFetchRetries < 1) {
+    console.warn("--maxFetchRetries can't be a negative number")
+    process.exit(1)
+}
 
 const getAsText = async (url) => {
     const result = await fetch(url, {
@@ -61,7 +66,7 @@ const get = async (url) => {
 
 const parseCsvWithHeaders = async (csvAsString) => {
     return new Promise((resolve, reject) => {
-        const rows = []
+        const instruments = []
         const stream = parse({
                 headers: true
             })
@@ -69,11 +74,11 @@ const parseCsvWithHeaders = async (csvAsString) => {
             .on('data', row => {
                 delete row.last_price // this is always 0 from the KITE API
                 delete row.exchange // this is always inferred to be NFO
-                delete row.exchange_token // we don't need this for history as it is useless
-                rows.push(row)
+                delete row.exchange_token // we don't need this for history as it is useless after it's expiry
+                instruments.push(row)
             })
             .on('end', () => {
-                resolve(rows)
+                resolve(instruments)
             })
 
         stream.write(csvAsString)
@@ -81,36 +86,50 @@ const parseCsvWithHeaders = async (csvAsString) => {
     })
 }
 
-const filterOnlyNiftyBankNiftyAndFinNiftyData = async (rows) => {
-    return rows.filter(row => {
-        return row.name === 'NIFTY' || row.name === 'BANKNIFTY' || row.name === 'FINNIFTY'
+const filterOnlyNiftyBankNiftyAndFinNiftyData = async (instruments) => {
+    return instruments.filter(instrument => {
+        return instrument.name === 'NIFTY' || instrument.name === 'BANKNIFTY' || instrument.name === 'FINNIFTY'
     })
 }
 
-const fetchAndWriteData = async (rows) => {
+const fetchAndWriteData = async (instruments) => {
     // don't change these values, these are as per the limits for Kite
     // Ref - https://kite.trade/forum/discussion/2760/no-of-request-to-api#latest
     // While the max is 3 requests / second for historical API, we do 2 just to be under the radar
     const throttledFetch = Throttle({
         limit: 2,
         interval: 1000
-    })(async (row, token, fromDate, toDate) => {
-        const result = await get(`https://api.kite.trade/instruments/historical/${token}/minute?from=${fromDate}&to=${toDate}&oi=1`);
-        if (!result.data) {
-            throw new Error(result.message)
-        }
+    })(async (row, token, fromDate, toDate, fetchAttempt) => {
+        const url = `https://api.kite.trade/instruments/historical/${token}/minute?from=${fromDate}&to=${toDate}&oi=1`
+        try {
+            const result = await get(url);
+            if (!result.data) {
+                throw new Error(result.message)
+            }
 
-        return {
-            ...result,
-            ...row,
+            return {
+                ...result,
+                ...row,
+            }
+        } catch (err) {
+            if (fetchAttempt < 3) {
+                console.warn(`Attempting to fetch ${url} once again. We'll try it ${3 - fetchAttempt} more times, before bailing out.`)
+                return await throttledFetch(row, token, fromDate, toDate, fetchAttempt + 1 || 2)
+            } else {
+                throw err
+            }
         }
     })
 
-    const requests = rows.map(row => {
-        // TODO: We should at somepoint support ability to fetch more data more than 60 days by spliting it into multiple requests of 60 days each
+    const requests = instruments.slice(0, 5).filter(instrument => {
+        const directory = directoryForInstrument(instrument)
+        const markerFile = markerFileForInstrument(instrument)
+        return !fs.existsSync(path.resolve(directory, markerFile))
+    }).map(instrument => {
+        // TODO: We should at somepoint support ability to fetch more data more than 60 days by spliting it into multiple requests of 60 days each?
         const fromDate = today.subtract(maxDays, 'days').format(DATE_FORMAT)
         const toDate = todayAsStr
-        return throttledFetch(row, row.instrument_token, fromDate, toDate).then(writeDataToCsv)
+        return throttledFetch(instrument, instrument.instrument_token, fromDate, toDate, 1).then(writeData)
     })
 
     // Don't remove the tickers which doesn't have any data.
@@ -118,28 +137,37 @@ const fetchAndWriteData = async (rows) => {
     return Promise.all(requests)
 }
 
+const directoryForInstrument = (instrument) => path.resolve(__dirname, "data", instrument.name, todayAsStr)
+const dataFileForInstrument = (instrument) => `${instrument.name}_${instrument.tradingsymbol}_${instrument.expiry}.csv`
+const markerFileForInstrument = (instrument) => `${dataFileForInstrument(instrument)}.done`
+
 // unlike the other method we call this after each fetch so we can write
 // as we fetch and not wait unitl we've fetched everything.
 // reduces the memory usage of the program
-const writeDataToCsv = async (instrument) => {
-    const directory = path.resolve(__dirname, "data", instrument.name, todayAsStr)
+const writeData = async (instrument) => {
+    const directory = directoryForInstrument(instrument)
     fs.mkdirSync(directory, {
         recursive: true
     })
 
-    const filename = `${instrument.name}_${instrument.tradingsymbol}_${instrument.expiry}.csv`
-    const fullPath = path.resolve(directory, filename)
+    const dataFilename = dataFileForInstrument(instrument)
+    const markerFilename = path.resolve(directory, markerFileForInstrument(instrument))
+    const dataFullPath = path.resolve(directory, dataFilename)
 
     await new Promise((resolve, reject) => {
-        writeToPath(fullPath, instrument.data.candles, {
+        writeToPath(dataFullPath, instrument.data.candles, {
                 alwaysWriteHeaders: true,
                 headers: ["timestamp", "open", "high", "low", "close", "volume", "oi"]
             })
             .on('error', err => reject(err))
-            .on('finish', () => resolve())
+            .on('finish', () => {
+                // write the marker once the data is written properly to disk
+                fs.closeSync(fs.openSync(markerFilename, 'w'))
+                resolve()
+            })
     })
 
-    console.log(`Done writing ${filename}`)
+    console.log(`Done writing ${dataFilename}`)
 }
 
 // Entry point of the program
